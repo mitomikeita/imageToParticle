3eba9b0c645eed07787fc3eaae14ec4715fe71f3
mitomikeita <keita.mitomi@gmail.com>
2011-09-21 23:50:06 +0900

    修正

--- emptyExample/src/testApp.cpp
+++ emptyExample/src/testApp.cpp
@@ -20,7 +20,6 @@ void testApp::setup(){
 	mouse_img_inside = false;
 	gravity = 0.1; //重力
     friction = 1;  //摩擦
-	
     
 	// imageの高さと幅を元に、ピクセル数だけスピード、座標、大きさをベクターに格納する
 	pixels = img.getPixels();
@@ -44,7 +43,7 @@ void testApp::setup(){
 			circle_size_R.push_back(10.0*(float)r/255.0); //色：Rから演算する円の大きさ
 			circle_size_G.push_back(10.0*(float)g/255.0); //色：Gから演算する円の大きさ
 			circle_size_B.push_back(10.0*(float)b/255.0); //色：Bから演算する円の大きさ
-			//imageを中央に持っていくvectorの初期化
+			//imageを中央に持っていく際のvectorの初期化
 			loc_x_forCenter.push_back(0);
 			loc_y_forCenter.push_back(0);
 		}
@@ -53,64 +52,56 @@ void testApp::setup(){
 
 //--------------------------------------------------------------
 void testApp::update(){
+    
 	ofBackground(0,0,0);
 	
-	// 初期表示（ピクセルがimageを構成）
-	
+	// 初期表示
 	if (!mouse_pressed) {
 		
 		for (int i=0; i < w; i++) {
 			for (int j=0; j < h; j++) {
 				
-				//マウスの現在位置から円のスピードを再計算
-				speed_x[j+h*i] = (loc_original_x[j+h*i] - loc_x[j+h*i])  / 8.0; //マウスのx座標と円のx座標の距離の1/8だけ接近
+				//particleの初期配置位置と現在位置の距離を8で割ったものをスピードとする
+				speed_x[j+h*i] = (loc_original_x[j+h*i] - loc_x[j+h*i])  / 8.0; 
 				speed_y[j+h*i] = (loc_original_y[j+h*i] - loc_y[j+h*i]) / 8.0;
 				
-				//　摩擦の計算
-				speed_x[j+h*i] = speed_x[j+h*i] * friction;
-				speed_y[j+h*i] = speed_y[j+h*i] * friction;
-				// 重力の計算
-				//speed_y[j+h*i] = speed_y[j+h*i] + gravity;
+                //particleの座標を更新
+				loc_x[j+h*i] = loc_x[j+h*i] + speed_x[j+h*i]; 
+				loc_y[j+h*i] = loc_y[j+h*i] + speed_y[j+h*i];
 				
-				loc_x[j+h*i] = loc_x[j+h*i] + speed_x[j+h*i]; //円のx座標を更新
-				loc_y[j+h*i] = loc_y[j+h*i] + speed_y[j+h*i]; //円のy座標を更新
-				
-				// imageを画面中央に持っていく処理。現在のpointに、ディスプレイの半分を加算し、構成している最後のピクセルのpointの半分を減算する
+				// particleの画面中央部に持ってく座標を更新
 				loc_x_forCenter[j+h*i] = loc_x[j+h*i] + (ofGetWidth()/2 - loc_x.back()/2);
 				loc_y_forCenter[j+h*i] = loc_y[j+h*i] + (ofGetHeight()/2 - loc_y.back()/2);
 			}
 		}
 	}
+    // click 
 	else {
 		for (int i=0; i < w; i++) {
 			for (int j=0; j < h; j++) {
 				
 				speed_x[j+h*i] = speed_x[j+h*i] * friction;
 				speed_y[j+h*i] = speed_y[j+h*i] * friction;
-				//	speed_y[j+h*i] = speed_y[j+h*i] + gravity;
 				
-				loc_x[j+h*i] = loc_x[j+h*i] + speed_x[j+h*i]; //円のx座標を更新
-				loc_y[j+h*i] = loc_y[j+h*i] + speed_y[j+h*i]; //円のy座標を更新
+                //particleの座標を更新
+				loc_x[j+h*i] = loc_x[j+h*i] + speed_x[j+h*i];
+				loc_y[j+h*i] = loc_y[j+h*i] + speed_y[j+h*i];
 				
-				// imageを画面中央に持っていく処理。現在のpointに、ディスプレイの半分を加算し、構成している最後のピクセルのpointの半分を減算する
+				// particleの画面中央部に持ってく座標を更新
 				loc_x_forCenter[j+h*i] = loc_x[j+h*i] + (ofGetWidth()/2 - loc_x.back()/2);
 				loc_y_forCenter[j+h*i] = loc_y[j+h*i] + (ofGetHeight()/2 - loc_y.back()/2);
 				
-				//円の跳ね返り条件
+				//particleの跳ね返り条件
 				if(loc_x_forCenter[j+h*i] - circle_size_R[j+h*i]/2 < 0){
-					//loc_x_forCenter = 0;
 					speed_x[j+h*i] = speed_x[j+h*i] * -1.0;
 				}
 				if(loc_x_forCenter[j+h*i] + circle_size_R[j+h*i]/2 > ofGetWidth()){
-					//	loc_x_forCenter = ofGetWidth();
 					speed_x[j+h*i] = speed_x[j+h*i] * -1.0;
 				}
 				if(loc_y_forCenter[j+h*i] - circle_size_R[j+h*i]/2 < 0){
-					//loc_y_forCenter = 0;
 					speed_y[j+h*i] = speed_y[j+h*i] * -1.0;
 				}
 				if(loc_y_forCenter[j+h*i] + circle_size_R[j+h*i]/2 > ofGetHeight()){
-					//loc_y_forCenter = ofGetHeight();
 					speed_y[j+h*i] = speed_y[j+h*i] * -1.0;
 				}	
 			}
@@ -123,17 +114,19 @@ void testApp::update(){
 void testApp::draw(){
 	ofSetColor(255, 255, 255);
 	
+    // mouseがimageの外側にある場合、元画像を描画」
 	if (!mouse_img_inside) {
         
 		img_nature.draw(ofGetWidth()/2 - img_nature.width/2,ofGetHeight()/2 - img_nature.height/2);
 	}
 	else {
 		
-        
+        // 非クリック時
 		if (!mouse_pressed) {
 			for (int i=0; i < w; i++) {
 				for (int j=0; j < h; j++) {
                     
+                    // mouseの近くのparticleは暗めに描画し、クリックをアフォードする
 					if ( ((mouseX - 12) < loc_x_forCenter[j+h*i] && (mouseX + 12) > loc_x_forCenter[j+h*i]) &&
                         ((mouseY - 12) < loc_y_forCenter[j+h*i] && (mouseY + 12) > loc_y_forCenter[j+h*i]) 
 						)
@@ -147,9 +140,7 @@ void testApp::draw(){
                         
 					}
 					else {
-                        
-						// ピクセルサークルの描画。それぞれの変数はdraw()内で定義されている
-						ofSetColor(255, 0, 0, 100);
+                        ofSetColor(255, 0, 0, 100);
 						ofCircle(loc_x_forCenter[j+h*i], loc_y_forCenter[j+h*i], circle_size_R[j+h*i]);
 						ofSetColor(0, 255, 0, 100);
 						ofCircle(loc_x_forCenter[j+h*i], loc_y_forCenter[j+h*i], circle_size_G[j+h*i]);
@@ -160,6 +151,7 @@ void testApp::draw(){
 				}
 			}
 		}
+        // クリック時
 		else {
 			for (int i=0; i < w; i++) {
 				for (int j=0; j < h; j++) {
@@ -176,29 +168,11 @@ void testApp::draw(){
 	}
 }
 
-// imageを描画する無意味な関数
-void testApp::imageDraw2(int size, ofImage img){
-	ofSetColor(255, 255, 255);
-	int w = size;
-	
-	img.resize(img.width*size, img.height*size);
-	img.draw(0, img.height);
-	img.resize(img.width/size, img.height/size);
-}
-void testApp::setTest(int setTest){
-	test = setTest;
-}
-int testApp::getTest(){
-	return test;
-}
-
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
     
 }
-
-
 
 
 //--------------------------------------------------------------
@@ -209,6 +183,7 @@ void testApp::keyReleased(int key){
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
 	
+    // mouseが元画像内部にいたらflagをtrueにする
 	if ((x >= ofGetWidth()/2 - img_nature.width/2 && x <= ofGetWidth()/2 + img_nature.width/2) &&
         y >= ofGetHeight()/2 - img_nature.height/2 && y <= ofGetHeight()/2 + img_nature.height/2) {
         
@@ -228,9 +203,12 @@ void testApp::mouseDragged(int x, int y, int button){
 void testApp::mousePressed(int x, int y, int button){
     
 	if (mouse_pressed){
+        
 		mouse_pressed = false;
-	}else if( (x >= ofGetWidth()/2 - img_nature.width/2 && x <= ofGetWidth()/2 + img_nature.width/2) &&
+	}
+    else if( (x >= ofGetWidth()/2 - img_nature.width/2 && x <= ofGetWidth()/2 + img_nature.width/2) &&
              y >= ofGetHeight()/2 - img_nature.height/2 && y <= ofGetHeight()/2 + img_nature.height/2) {
+        
 		mouse_pressed = true;
 		
 		// speedのinitialize
